997fb9e2fbe09025176481b8e255cee58daa6273
coderanishka <62501236+coderanishka@users.noreply.github.com>
2020-04-29 14:02:38 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,11 +10,11 @@ var form, player, game;
 
 var runners, runner1, runner2, runner3, runner4;
 
-var track, runner1_img, runner2_img, runner3_img, runner4_img;
+var track;
 
 function preload(){
   track = loadImage("images/track.jpg");
-  ground = loadImage("images/ground.png");
+  
 }
 
 function setup(){
@@ -27,7 +27,11 @@ function setup(){
 
 
 function draw(){
-  background(ground);
+  background(0);
+  spawnHurdles(250);
+  spawnHurdles(350);
+  spawnHurdles(450);
+  spawnHurdles(550);
   if(playerCount === 4){
     game.update(1);
   }
@@ -37,6 +41,12 @@ function draw(){
   }
   if(gameState === 2){
     game.end();
+  }
+}
+
+function spawnHurdles(y){
+  for (var i = 1500; i<4500; i+=500){
+    hurdle = createSprite(i,y,30,70);
   }
 }
 
