083ef77986886065df33544ff8a4fd8ce678c1f1
coderanishka <62501236+coderanishka@users.noreply.github.com>
2020-06-25 16:35:28 +0530

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -7,14 +7,14 @@ class Form {
     this.title = createElement('h2');
   }
   hide(){
-    this.greeting.hide();
+   // this.greeting.hide();
     this.button.hide();
     this.input.hide();
     this.title.hide();
   }
 
   display(){
-    this.title.html("runner Racing Game");
+    this.title.html("Horizontal Runners");
     this.title.position(displayWidth/2 - 50, 0);
 
     this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
@@ -28,7 +28,7 @@ class Form {
       player.index = playerCount;
       player.update();
       player.updateCount(playerCount);
-      this.greeting.html("Hello " + player.name)
+      this.greeting.html("Hello " + player.name+"Please wait for the other players to join in.")
       this.greeting.position(displayWidth/2 - 70, displayHeight/4);
     });
 
